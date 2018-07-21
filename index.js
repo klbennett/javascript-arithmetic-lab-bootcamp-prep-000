f996a3539213fcecff0c01a80f3ab3aff22bce73
thestylishkidsintheriot@gmail.com <thestylishkidsintheriot@gmail.com>
2018-07-21 12:06:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  var addition = a + b;
+  return addition;
+}
+
+add(1 + 80);
+
+function subtract(a, b) {
+  var subtract = a - b;
+  return subtract;
+}
+
+subtract(60 - 40);
+
+function multiply(a, b) {
+  var multiply = a * b;
+  return multiply;
+}
+
+multiply(2 * 3.4);
+
+function multiply(a, b) {
+  var multiply = a * b;
+  return multiply;
+}
+
+divide(5 / 2.5);
+ var divide = 5 / 2.5;
+ return divide;
+ }
+ 
+ inc(n)
+
